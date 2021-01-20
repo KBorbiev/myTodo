cfb1fe326838420090e3562abcb9e48a9cecce31
Kairat <mailforstudy.kg@gmail.com>
2021-01-21 00:41:07 +0600

    add book module and make migrations

--- main/models.py
+++ main/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django import forms
 
 class ToDo(models.Model):
     text = models.CharField(max_length=100)
     created_at = models.DateField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=80)
+    description = models.CharField(max_length=200)
+    price = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    year = models.DateField(auto_now_add=True)
+    date = forms.DateTimeField(input_formats=['%d/%m/%Y'], widget=forms.DateTimeInput(format='%d/%m/%Y'))
